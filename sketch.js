1a50e9d49ff7de3fb9a701d315fa81b76c83b4ad
Mary Notari <32004486+marynotari@users.noreply.github.com>
2018-03-07 15:21:57 -0500

    create sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+/* 
+		CREDITS:
+		
+    Moment.js to calculate time 
+    	-- http://momentjs.com/
+      
+    Data source: MARK FOLLMAN, GAVIN ARONSEN and DEANNA PAN for MOTHER JONES 
+    	-- https://www.motherjones.com/politics/2012/12/mass-shootings-mother-jones-full-data/
+      
+    Tone.js by Yotam Mann
+    	-- https://tonejs.github.io/
+      
+    Dies Irae by W.A. Mozart
+    	-- Sheet Music Reference: http://www.free-scores.com/download-sheet-music.php?pdf=15184
+*/
+
+// var button;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+
+function draw() {
+
+}
